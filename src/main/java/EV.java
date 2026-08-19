e71e06be6a67997c28aadcfd8b4c6500fe7cd721
jon <144509743+aerodart@users.noreply.github.com>
2026-08-20 00:02:54 +0800

    Complete Level-1

--- src/main/java/EV.java
+++ src/main/java/EV.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class EV {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("E.V. online.");
         System.out.println("Good evening, Jonathan.");
         System.out.println("Systems are operational. What can I do for you?");
         System.out.println();
-        System.out.println("Standing by.");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        System.out.println();
+        System.out.println("E.V. offline. Until next time, Jonathan.");
+
+        scanner.close();
     }
 }
