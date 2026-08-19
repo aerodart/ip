ddb454a869ac521762de72450c0e9cf21283f8c2
jon <144509743+aerodart@users.noreply.github.com>
2026-08-19 23:56:30 +0800

    Complete Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# EV User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/EV.java
@@ -0,0 +1,9 @@
+public class EV {
+    public static void main(String[] args) {
+        System.out.println("E.V. online.");
+        System.out.println("Good evening, Jonathan.");
+        System.out.println("Systems are operational. What can I do for you?");
+        System.out.println();
+        System.out.println("Standing by.");
+    }
+}
