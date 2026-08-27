ed054165a375645a6c678f54ee07ac78da504d4e
jon <144509743+aerodart@users.noreply.github.com>
2026-08-28 02:50:51 +0800

    Add newline at end of source files

---
+++ runtest.sh
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+rm -rf out
+javac -d out src/main/java/ev/*.java src/main/java/ev/task/*.java
+java -cp out ev.EV < testcases.txt > actual.txt
+diff expected.txt actual.txt && echo PASS
