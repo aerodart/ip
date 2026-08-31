ac79735050f6ccbecf5011646448d52b7062d2be
jon <144509743+aerodart@users.noreply.github.com>
2026-09-01 00:18:54 +0800

    Add unit tests for TaskList and Storage

---
+++ src/test/java/ev/StorageTest.java
@@ -0,0 +1,68 @@
+package ev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import ev.task.Deadline;
+import ev.task.Task;
+import ev.task.Todo;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ * Contains unit tests for {@link Storage}.
+ */
+public class StorageTest {
+    @TempDir
+    Path tempDir;
+
+    @Test
+    public void load_fileDoesNotExist_returnsEmptyList() throws EvException {
+        Storage storage = new Storage(tempDir.resolve("missing.txt").toString());
+
+        assertEquals(0, storage.load().size());
+    }
+
+    @Test
+    public void saveThenLoad_todoAndDeadline_surviveRoundTrip() throws EvException {
+        Storage storage = new Storage(tempDir.resolve("ev.txt").toString());
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("borrow book"));
+        tasks.add(new Deadline("return book", LocalDateTime.of(2026, 9, 18, 18, 0)));
+        tasks.get(0).markAsDone();
+
+        storage.save(tasks);
+
+        ArrayList<Task> loaded = storage.load();
+
+        assertEquals(2, loaded.size());
+        assertEquals("[T][X] borrow book", loaded.get(0).toString());
+        assertEquals("[D][ ] return book (by: Sep 18 2026, 6:00PM)", loaded.get(1).toString());
+    }
+
+    @Test
+    public void load_unknownTaskType_throwsEvException() throws IOException {
+        Path file = tempDir.resolve("ev.txt");
+        Files.writeString(file, "X | 0 | not a real task type\n");
+
+        Storage storage = new Storage(file.toString());
+
+        assertThrows(EvException.class, storage::load);
+    }
+
+    @Test
+    public void load_tooFewFields_throwsEvException() throws IOException {
+        Path file = tempDir.resolve("ev.txt");
+        Files.writeString(file, "T | 0\n");
+
+        Storage storage = new Storage(file.toString());
+
+        assertThrows(EvException.class, storage::load);
+    }
+}
---
+++ src/test/java/ev/TaskListTest.java
@@ -0,0 +1,70 @@
+package ev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ev.task.Todo;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Contains unit tests for {@link TaskList}.
+ */
+public class TaskListTest {
+    @Test
+    public void add_threeTasks_sizeReflectsCount() {
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("borrow book"));
+        tasks.add(new Todo("return book"));
+        tasks.add(new Todo("buy milk"));
+
+        assertEquals(3, tasks.size());
+    }
+
+    @Test
+    public void remove_middleTask_returnsItAndShrinksList() {
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("first"));
+        tasks.add(new Todo("second"));
+        tasks.add(new Todo("third"));
+
+        assertEquals("[T][ ] second", tasks.remove(1).toString());
+        assertEquals(2, tasks.size());
+        assertEquals("[T][ ] third", tasks.get(1).toString());
+    }
+
+    @Test
+    public void find_matchingKeyword_returnsOnlyMatches() {
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("borrow book"));
+        tasks.add(new Todo("return book"));
+        tasks.add(new Todo("buy milk"));
+
+        TaskList matches = tasks.find("book");
+
+        assertEquals(2, matches.size());
+        assertEquals("[T][ ] borrow book", matches.get(0).toString());
+        assertEquals("[T][ ] return book", matches.get(1).toString());
+    }
+
+    @Test
+    public void find_noMatch_returnsEmptyList() {
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("borrow book"));
+
+        assertEquals(0, tasks.find("spiderman").size());
+    }
+
+    @Test
+    public void find_doesNotModifyOriginalList() {
+        TaskList tasks = new TaskList();
+
+        tasks.add(new Todo("borrow book"));
+        tasks.add(new Todo("buy milk"));
+        tasks.find("book");
+
+        assertEquals(2, tasks.size());
+    }
+}
